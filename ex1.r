de2bc7871ccc3d86e08ca05b3d2ad964901c7308
Jorge Vitor <103287884+JorgeVitor30@users.noreply.github.com>
2023-02-02 19:37:56 -0300

    Introdução ao R

---
+++ ex1.r
@@ -0,0 +1,63 @@
+
+OPERADORES DE R
+
+```{r}
+delta = 8
+class(delta)
+
+```
+
+```{r}
+delta = 8L
+class(delta)
+
+
+```
+```{r}
+logico = TRUE
+logico = FALSE
+
+caractere = "Texto"
+caractere = 'Texto'
+class(logico)
+
+```
+
+OPERAÇÕES MATEMÁTICAS
+
+```{r}
+c = 2
+d = 4
+(c + d) * d
+e = (c * d) * d
+e
+
+
+```
+
+```{r}
+a = 1
+b = 2
+a < b
+a == b
+
+
+```
+```{r}
+sqrt(2500)
+
+
+```
+
+
+
+
+
+
+
+
+
+
+
+
+
