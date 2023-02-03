b631e0440fe0f61e3cf0d9b81109c3ad5a96d8ff
Jorge Vitor <103287884+JorgeVitor30@users.noreply.github.com>
2023-02-03 20:41:21 -0300

    Comandos básicos de R

---
+++ ex2.r
@@ -0,0 +1,41 @@
+1,2,3,4
+```{r}
+Ana = 8L
+Paulo = 12L
+
+x = ifelse(Ana>Paulo, "Ana mais velha", "Paulo mais velho")
+x
+
+class(BOD)
+
+v = c(1,2,3,4,5,6,7,8,9,10)
+
+for (x in v){
+  if (x %% 2 == 0){
+    print(x)
+  }
+}
+
+tail(women,10)
+
+
+```
+
+5,6,7
+```{r}
+
+#plot(iris,iris)
+
+a = c(1,2,3)
+b = c(4,5,6)
+(a + b)
+
+dim(CO2)[1]
+rownames(CO2)
+dim(CO2)[2]
+colnames(CO2)
+
+
+```
+
+
